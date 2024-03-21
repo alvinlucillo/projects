4008a2158a4c555b7ca65f93185a78dd011724a0
alvinlucillo <allouothers@gmail.com>
2024-03-21 19:00:43 +0800

    Add sample handler

--- healthstats/server/pkg/router/router.go
+++ healthstats/server/pkg/router/router.go
@@ -1,11 +1,41 @@
 package router
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+type router struct {
+	router *http.ServeMux
+}
 
 func NewRouter() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world!"))
+	r := &router{router: http.NewServeMux()}
+
+	r.setupRoutes()
+
+	return r.router
+}
+
+func (r *router) setupRoutes() {
+	r.router.HandleFunc("POST /upload", func(w http.ResponseWriter, req *http.Request) {
+		err := req.ParseMultipartForm(10 << 20) // 10 MB
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Retrieve the file from form data.
+		file, handler, err := req.FormFile("file") // "file" is the key of the form data
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+
+		// You can now use the file, for example, save it to disk.
+		// For now, let's just respond with the name of the file.
+		w.Write([]byte(fmt.Sprintf("Successfully uploaded file: %s", handler.Filename)))
 	})
-	return mux
+
 }
